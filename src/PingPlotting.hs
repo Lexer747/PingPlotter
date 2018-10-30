0bf81dc581fbab9117d5f762d1bf20214bdad001
Lexer747 <Lexer747@gmail.com>
2018-10-30 14:17:25 +0000

    found issue causing the ping to be recalculated each cycle

--- src/PingPlotting.hs
+++ src/PingPlotting.hs
@@ -22,25 +22,9 @@ pingCycle graph = do
                     pingCycle $! addPing graph --perform a strict evaluation
             
             
-getSample :: String -> IO()
-getSample host = do
-                    let g1 = addPing $ getInitGraph host 
-                    g1' <- g1
-                    graphPrint id fromIntegral g1'
-                    let g2 = addPing g1
-                    g2' <- g2
-                    graphPrint id fromIntegral g2'
-                    let g3 = addPing g2
-                    g3' <- g3
-                    graphPrint id fromIntegral g3'
-                    let g4 = addPing g3
-                    g4' <- g4
-                    graphPrint id fromIntegral g4'
-                    let g5 = addPing g4
-                    g5' <- g5
-                    graphPrint id fromIntegral g5'
-                    i <- toInternal id fromIntegral g5'
-                    case i of 
-                        Just x -> putStr $ show x
-                        Nothing -> putStr "ops"
-                    
+getSample :: String -> IO (Graph TimeStamp Integer)
+getSample host = cycle' init
+    where init = getInitGraph host
+
+cycle' :: IO (Graph TimeStamp Integer) -> IO (Graph TimeStamp Integer)
+cycle' graph = cycle' $! addPing $! graph
