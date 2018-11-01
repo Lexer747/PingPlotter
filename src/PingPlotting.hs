4dd2676b91825133b3610880ea40008e65b85037
Lexer747 <Lexer747@gmail.com>
2018-11-01 12:20:16 +0000

    first try at fixing the laziness building up in the graph data

--- src/PingPlotting.hs
+++ src/PingPlotting.hs
@@ -7,10 +7,10 @@ import GraphPlotter
 import GraphBuild
 import InternalGraph
 
-partialPlot :: Graph TimeStamp Integer -> IO ()
-partialPlot g = do
-                    clear
-                    graphPrint id fromIntegral g
+import Control.Monad.State.Strict
+
+{-
+
 
 start :: String -> IO ()
 start host = pingCycle $ getInitGraph host
@@ -28,3 +28,29 @@ getSample host = cycle' init
 
 cycle' :: IO (Graph TimeStamp Integer) -> IO (Graph TimeStamp Integer)
 cycle' graph = cycle' $! addPing $! graph
+-}
+
+type GraphState = State (IO (Graph TimeStamp Integer)) (IO ())
+
+startCycle :: String -> IO ()
+startCycle host = plottingCycle $ getInitGraph host
+
+plottingCycle :: IO (Graph TimeStamp Integer) -> IO ()
+plottingCycle state = do
+                    let (io, newState) = runState evalGraphState state
+                    io
+                    plottingCycle newState
+
+evalGraphState :: GraphState
+evalGraphState = do
+    graph <- get
+    put $ addPing graph
+    return (do
+                g <- graph
+                partialPlot g)
+
+    
+partialPlot :: Graph TimeStamp Integer -> IO ()
+partialPlot g = do
+    clear
+    graphPrint id fromIntegral g
