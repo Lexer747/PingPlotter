5a66338ee168677a6080b02ade17f5881fad75e6
Lexer747 <Lexer747@gmail.com>
2018-12-15 08:31:34 +0000

    it actually works

--- src/Files/CoreIO.hs
+++ src/Files/CoreIO.hs
@@ -3,11 +3,20 @@ module Files.CoreIO where
 import Graph.Types
 import Ping.Types
 import Ping.Display
+import Ping.Graph
 import Files.Serialization
 
 
 mainLoop :: String -> IO ()
-mainLoop = undefined
+mainLoop host = innerLoop $ getInitGraph host
+
+innerLoop :: IO (Graph TimeStamp Integer) -> IO ()
+innerLoop graph = do
+                    g <- addPing graph
+                    saveGraph g
+                    newG <- readPingGraph (saveLocation g)
+                    drawGraph newG
+                    innerLoop $ return newG
 
 mainInstance :: String -> IO ()
 mainInstance file = do
