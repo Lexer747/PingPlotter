4f49dff5f4794558945f24c0f7a2f48a041636aa
Lexer747 <Lexer747@gmail.com>
2018-12-16 09:43:37 +0000

    trying to buffer the graph drawing properly

--- src/Files/CoreIO.hs
+++ src/Files/CoreIO.hs
@@ -2,6 +2,7 @@ module Files.CoreIO where
 
 import Graph.Types
 import Graph.Plotter
+import Graph.Internal
 import Ping.Types
 import Ping.Graph
 import Ping.API
@@ -9,6 +10,7 @@ import Files.Serialization
 
 import System.IO
 import Control.Concurrent
+import Data.Bits (shiftR)
 
 graphPrint :: (Show a, Show b, IOShow a, IOShow b, RealFrac x, Enum x, Ord x) =>
     (a -> x) -> (b -> x) -> Graph a b -> IO ()
@@ -16,26 +18,37 @@ graphPrint cX cY g = do
     maybePlot <- graphToPlot cX cY g
     case maybePlot of
         Nothing -> buffer "graphPrint Failed - cause: window size probably failed"
-        Just plot -> buffer $ plotToPrintString plot
+        Just plot -> buffer $ (show $ length $ plotToPrintString plot) ++ " - " ++plotToPrintString plot
 
 drawGraph :: Graph TimeStamp Integer -> IO ()
 drawGraph g = do
     graphPrint id fromIntegral g
 
-bufferSize :: Int
-bufferSize = 2 ^ 18
+findBuffer :: Int -> Int
+findBuffer b | b < 16   = 4
+findBuffer b            = 1 +  (findBuffer $ b `shiftR` 1)
 
-editStdout :: IO ()
-editStdout = hSetBuffering stdout (BlockBuffering $ Just bufferSize)
+--fullscreen ~15000 chars (62 * 236)
+--half screen ~9000 chars (62 * 138)
+setBuffer :: Int -> IO ()
+setBuffer i = hSetBuffering stdout (BlockBuffering $ Just i)
+
+adjustBuffer :: String -> IO ()
+adjustBuffer s = let size = (length s) * 64 in
+                 do
+                    bufferMode <- hGetBuffering stdout
+                    case bufferMode of
+                        BlockBuffering (Just x) | x < size -> setBuffer $ 2 ^ (findBuffer size)
+                        _                                  -> return ()
 
 buffer :: String -> IO ()
 buffer s = do
+            adjustBuffer s
             putStr s
             hFlush stdout
 
 mainLoop :: String -> IO ()
 mainLoop host = do
-                    editStdout
                     innerLoop $ getInitGraph host
 
 innerLoop :: IO (Graph TimeStamp Integer) -> IO ()
@@ -44,9 +57,20 @@ innerLoop graph = do
                     saveGraph g
                     newG <- readPingGraph (saveLocation g)
                     threadDelay 50
-                    --clear
                     drawGraph newG
                     innerLoop $ return newG
+
+singleLoop :: String -> IO ()
+singleLoop host = do
+                    g <- addPing $ addPing $ addPing $ getInitGraph host
+                    saveGraph g
+                    newG <- readPingGraph (saveLocation g)
+                    threadDelay 50
+                    drawGraph newG
+                    maybeG <- toInternal id fromIntegral newG
+                    case maybeG of
+                        Just gg -> buffer $ (show $ window gg)
+                        Nothing -> return ()
 
 mainInstance :: String -> IO ()
 mainInstance file = do
